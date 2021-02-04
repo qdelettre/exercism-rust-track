80b47e2c97ef587e290b979cb2dfc66631c5b271
Quentin DELETTRE <quentin.delettre@gmail.com>
2021-02-04 23:03:09 +0100

    High Scores

---
+++ high-scores/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut s = self.scores.to_vec();
+        s.sort_unstable_by(|a, b| b.cmp(a));
+        s.truncate(3);
+
+        s
+    }
+}
