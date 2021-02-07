b88afa1f2904b88d1441ac83b591002bd54b654b
Quentin DELETTRE <quentin.delettre@gmail.com>
2021-02-07 17:45:11 +0100

    Matching brackets

---
+++ matching-brackets/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut stack: Vec<char> = Vec::new();
+    for c in string.chars() {
+        match c {
+            '(' | '{' | '[' => stack.push(c),
+            ')' if stack.pop() != Some('(') => {
+                return false;
+            }
+            ']' if stack.pop() != Some('[') => {
+                return false;
+            }
+            '}' if stack.pop() != Some('{') => {
+                return false;
+            }
+            _ => continue,
+        }
+    }
+    stack.is_empty()
+}
