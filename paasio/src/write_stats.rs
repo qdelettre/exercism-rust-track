dfa3469bd24b50e8543ee4a5e5547d9d82e8125e
Quentin DELETTRE <quentin.delettre@gmail.com>
2021-03-27 18:30:24 +0100

    PaaS I/O

---
+++ paasio/src/write_stats.rs
@@ -0,0 +1,46 @@
+use std::io::{Result, Write};
+
+pub struct WriteStats<W> {
+    wrapped: W,
+    bytes: usize,
+    writes: usize,
+}
+
+impl<W: Write> WriteStats<W> {
+    // _wrapped is ignored because W is not bounded on Debug or Display and therefore
+    // can't be passed through format!(). For actual implementation you will likely
+    // wish to remove the leading underscore so the variable is not ignored.
+    pub fn new(wrapped: W) -> WriteStats<W> {
+        Self {
+            wrapped,
+            bytes: 0,
+            writes: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &W {
+        &self.wrapped
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes
+    }
+
+    pub fn writes(&self) -> usize {
+        self.writes
+    }
+}
+
+impl<W: Write> Write for WriteStats<W> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        self.writes += 1;
+        self.wrapped.write(buf).and_then(|bytes| {
+            self.bytes += bytes;
+            Ok(bytes)
+        })
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.wrapped.flush()
+    }
+}
