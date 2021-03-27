f2e5ad4f8735afcac05e6493e9a6c92511b4745a
Quentin DELETTRE <quentin.delettre@gmail.com>
2021-03-27 18:30:24 +0100

    PaaS I/O

---
+++ paasio/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod read_stats;
+pub mod write_stats;
---
+++ paasio/src/read_stats.rs
@@ -0,0 +1,42 @@
+use std::io::{Read, Result};
+
+pub struct ReadStats<R> {
+    wrapped: R,
+    bytes: usize,
+    reads: usize,
+}
+
+impl<R: Read> ReadStats<R> {
+    // _wrapped is ignored because R is not bounded on Debug or Display and therefore
+    // can't be passed through format!(). For actual implementation you will likely
+    // wish to remove the leading underscore so the variable is not ignored.
+    pub fn new(wrapped: R) -> ReadStats<R> {
+        Self {
+            wrapped,
+            bytes: 0,
+            reads: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &R {
+        &self.wrapped
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes
+    }
+
+    pub fn reads(&self) -> usize {
+        self.reads
+    }
+}
+
+impl<R: Read> Read for ReadStats<R> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.reads += 1;
+        self.wrapped.read(buf).and_then(|bytes| {
+            self.bytes += bytes;
+            Ok(bytes)
+        })
+    }
+}
