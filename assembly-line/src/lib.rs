76ce45df7802c1b74baed3e3c5ab113b574a1380
Quentin Delettre <quentin.delettre@gmail.com>
2021-11-28 16:29:00 +0000

    assembly-line

---
+++ assembly-line/src/lib.rs
@@ -0,0 +1,17 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let success_rate: f64 = match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
+        _ => 0.0,
+    };
+    (speed as f64) * 221.0 * success_rate
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60.0) as u32
+}
