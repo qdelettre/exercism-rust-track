8c4650672f8bd7b524971eac1d7fb36afaacb6e9
Quentin Delettre <quentin.delettre@pm.me>
2022-02-27 19:54:27 +0100

    allergies

---
+++ allergies/src/lib.rs
@@ -0,0 +1,43 @@
+pub struct Allergies(pub u32);
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+static ALLERGENS: [Allergen; 8] = [
+    Allergen::Eggs,
+    Allergen::Peanuts,
+    Allergen::Shellfish,
+    Allergen::Strawberries,
+    Allergen::Tomatoes,
+    Allergen::Chocolate,
+    Allergen::Pollen,
+    Allergen::Cats,
+];
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Allergies(score)
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        let value = *allergen as u32;
+        value & self.0 != 0
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        ALLERGENS
+            .iter()
+            .filter(|a| self.is_allergic_to(a))
+            .map(|&n| n)
+            .collect()
+    }
+}
